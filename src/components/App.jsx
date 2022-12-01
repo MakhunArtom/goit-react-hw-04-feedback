8ba2ff9085a375e18cf2c5f28ff1efee3a3b7e9c
MakhunArtom <super_makhun@ukr.net>
2022-12-01 14:53:45 +0200

    featch/feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,57 +1,55 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import css from './App.module.css';
 import Controler from './Controler/Controler';
 import StaticsList from './StaticList/StaticList';
 import Notification from './Notification/Notification';
 import Section from './Secttion/section';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setVeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  countPositiveFeedbackPercentage = total => {
-    const { good } = this.state;
-
+  const countPositiveFeedbackPercentage = total => {
     return Math.round((good * 100) / total);
   };
 
-  onHendelFeedback = e => {
+  const onHendelFeedback = e => {
     const { name } = e.target;
 
-    this.setState(prevSteta => {
-      return {
-        [name]: prevSteta[name] + 1,
-      };
-    });
+    if (name === 'good') {
+      setGood(good + 1);
+    }
+
+    if (name === 'neutral') {
+      setVeutral(neutral + 1);
+    }
+
+    if (name === 'bad') {
+      setBad(bad + 1);
+    }
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const totalFeedback = bad + good + neutral;
+  const totalFeedback = bad + good + neutral;
+  return (
+    <div className={css.widget}>
+      <Section title="Please leave feedback">
+        <Controler onLiveFeedback={onHendelFeedback} />
+      </Section>
 
-    return (
-      <div className={css.widget}>
-        <Section title="Please leave feedback">
-          <Controler onLiveFeedback={this.onHendelFeedback} />
+      {!totalFeedback ? (
+        <Notification message="There is no feedback" />
+      ) : (
+        <Section title="Statics">
+          <StaticsList
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={totalFeedback}
+            feedbackProcent={countPositiveFeedbackPercentage}
+          />
         </Section>
-
-        {!totalFeedback ? (
-          <Notification message="There is no feedback" />
-        ) : (
-          <Section title="Statics">
-            <StaticsList
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={totalFeedback}
-              feedbackProcent={this.countPositiveFeedbackPercentage}
-            />
-          </Section>
-        )}
-      </div>
-    );
-  }
-}
+      )}
+    </div>
+  );
+};
